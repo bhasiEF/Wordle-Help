7178a3e5f401bcf628b156e8815e7d2db5da4e07
bhasiEF <99355176+bhasiEF@users.noreply.github.com>
2022-02-09 23:35:00 +0000

    Add files via upload

---
+++ WordRelations.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  4 13:09:39 2022
+
+@author: bhasi
+"""
+
+import numpy as np
+import string
+
+alphabet = list(string.ascii_lowercase)
+master_list = np.loadtxt('csw19.csv', dtype=str)
+num_words = len(master_list)
+
+#%%
+
+def tensorize(z):
+    chars = np.array([list(s) for s in z])
+    return np.moveaxis(np.array([(chars == alphabet[i]) for i in range(len(alphabet))], dtype=bool),[0,1,2], [2, 0, 1])
+
+def condense(z):
+    num1, num2 = np.shape(z)[:2]
+    z = np.reshape(z,(num1*num2, 25))
+    x = np.reshape(np.arange(0,25),(5,5))
+    
+    for n in range(5):
+        a = np.ones(25,dtype=bool)
+        a[np.setxor1d(x[n,:], x[:,n])] = False
+        z[z[:,x[n,n]]] = a*z[z[:,x[n,n]]==1]
+    
+    for n in range(5):
+        for m in np.setxor1d(x[0,:], n):   
+            a = np.ones(25,dtype=bool)
+            a[np.setxor1d(x[n,:], x[:,m])] = False
+            z[z[:,x[n,m]]] = a*z[z[:,x[n,m]]==1]
+    
+    y = np.zeros((np.shape(z)[0],5),dtype='uint8')
+    
+    for n in range(5):
+        a = np.setxor1d(x[n,:], 6*n)
+        y[:,n] = 2*(z[:,n*6]) + (z[:,a[0]]) + (z[:,a[1]]) + (z[:,a[2]]) + (z[:,a[3]])
+       
+    y = np.sum(np.array([81,27,9,3,1],dtype='uint8')*y,axis=1)
+    
+    return np.reshape(y,(num1, num2))
+
+#%%
+
+master_list_tensor = tensorize(master_list)
+inner_products = np.tensordot(master_list_tensor,master_list_tensor, axes=([2],[2])).swapaxes(1,2)
+del master_list_tensor
+
+relations = np.array(condense(inner_products), dtype='uint8')  
+del inner_products
+
+#%%
+uz = np.delete(np.arange(0,243),242-np.array([81,27,9,3,1]))
+
+# no prior knowledge of answer list
+prob = np.array([np.sum(word_relations == u,axis=1)/num_words for u in uz])
+log_prob = np.where(prob==0,0,np.log2(prob))
+info = -np.sum(prob*log_prob, axis=0)
+
+# prior knowledge of answer list
+prob2 = np.array([np.sum(word_relations[:,:2315] == u,axis=1)/2315 for u in uz])
+log_prob2 = np.where(prob2==0,0,np.log2(prob2))
+info2 = -np.sum(prob2*log_prob2, axis=0)
+
+#%%
+np.save('word_relations_csw19.npy', relations)
+np.save('seed_entropies_csw19_no_prior.npy', info)
+np.save('seed_entropies_csw19_prior_2315.npy', info2)
---
+++ WordleHelp.py
@@ -0,0 +1,149 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  4 18:24:55 2022
+
+@author: bhasi
+"""
+
+import numpy as np
+import string
+import requests
+import io
+
+alphabet = list(string.ascii_lowercase)
+
+nth = {
+    1: "first",
+    2: "second",
+    3: "third",
+    4: "fourth",
+    5: "fifth",
+    6: "sixth",
+}
+
+response = requests.get('https://raw.githubusercontent.com/bhasiEF/Wordle-Help/main/csw19.csv')
+response.raise_for_status()
+master_list = np.loadtxt(io.BytesIO(response.content),dtype=str)
+
+for i in range (0,8):  
+  response = requests.get(f'https://github.com/bhasiEF/Wordle-Help/blob/main/relations_csw19_{i}.npy?raw=true', allow_redirects=True)
+  response.raise_for_status()
+  if i == 0:
+    relations = np.load(io.BytesIO(response.content))
+  else:
+    relations = np.hstack((relations,np.load(io.BytesIO(response.content))))
+
+response = requests.get('https://github.com/bhasiEF/Wordle-Help/blob/main/seed_entropies_no_prior.npy?raw=true')
+response.raise_for_status()
+info = np.load(io.BytesIO(response.content))
+
+#%%
+
+def wordle_help():
+    status = 0
+    match = 242
+    round_num = 1
+    
+    sub = master_list
+    rel = relations
+    inf = info
+    
+    i = np.argmax(inf)
+    
+    print(f"Which word will you play as your {nth[round_num]} guess?")
+    print("Wordle helper recommends that you try \"", master_list[i], "\".", sep='')
+    print(f'We expect {inf[i]:2.2f} bits of information from guessing \"', master_list[i], "\".", sep='')
+    
+    temp = input()
+    while not temp in master_list:
+        print("This word is not on the master list of words on file. Please try another word. (If Wordle accepted the word, please update the master files)")
+        temp = input()
+    
+    print("You guessed \"", temp, "\".", sep='')
+    
+    if not temp == master_list[i]: 
+        i = np.argwhere(master_list == temp)[0][0]
+        print(f'We expect {inf[i]:2.2f} bits of information from guessing \"', temp, "\".", sep='')
+    
+    x,y = np.unique(rel[i,:],axis=0,return_inverse=True)
+    
+    print(f"Enter wordle output for \"{temp}\":")
+    
+    need_input = True
+    while need_input:
+        try:
+            status = int(input(), 3)
+            need_input = False
+        except:    
+            print(f"Please enter a valid wordle output for \"{temp}\".")
+            print("The output should be coded as a 5-digit number consisting of 0s,1s, and 2s")
+            print("0 corresponds to a letter that is not in the word.")
+            print("1 corresponds to a letter that is in the word, but in the incorrect position.")
+            print("2 corresponds to a letter in the correct position.")
+        
+    round_num += 1
+    
+    while not (status == match):
+        inds = np.where([np.all(r == status) for r in x])[0][0] == y
+
+        rel = rel[:,inds]
+        sub = sub[inds]
+
+        p = np.array([np.sum(rel == u,axis=1)/np.shape(rel)[1] for u in np.unique(rel)])
+        log_p = np.log2(np.where(p==0,1E-10,p))
+        inf = -np.sum(p*log_p, axis=0)
+        
+        print("Possible wordles still in the running:")
+        print(sub)
+        
+        i_s = np.squeeze(np.argwhere(inf == np.amax(inf)))
+        guess_candidates = master_list[i_s]
+        guess_overlap = [w for w in guess_candidates if w in sub]
+
+        
+        if len(guess_overlap) == 0 and np.ndim(i_s) > 0:
+            i = i_s[0]
+        elif len(guess_overlap) == 0:
+            i = int(i_s)
+        else:
+            i = np.argwhere(master_list == guess_overlap[0])[0][0]
+       
+        
+        if len(sub)>1:
+            print(f"Which word will you play as your {nth[round_num]} guess?")
+            print("Wordle helper recommends that you try \"", master_list[i], "\".", sep='')
+            print(f'We expect {inf[i]:2.2f} bits of information from guessing \"', master_list[i], "\".", sep='')
+            
+            temp = input()
+            
+            while not temp in master_list:
+                print("This word is not on the master list of words on file. Please try another word. (If Wordle accepted the word, please update the master files)")
+                temp = input()
+            
+            print("You guessed \"", temp, "\".", sep='')
+            
+   
+            if not temp == master_list[i]: 
+                i = np.argwhere(master_list == temp)[0][0]
+                print(f'We expect {inf[i]:2.2f} bits of information from guessing \"', temp, "\".", sep='')
+            
+        else:
+            print("Based on its word list, wordle helper has narrowed the possiblities down to one word: \"", sub[0], "\".", sep='')
+            temp = sub[0]
+        x,y = np.unique(rel[i,:,],axis=0,return_inverse=True)
+                
+        print("Enter wordle output:")
+        
+        need_input = True
+        while need_input:
+            try:
+                status = int(input(), 3)
+                need_input = False
+            except:    
+                print(f"Please enter a valid wordle output for \"{temp}\".")
+                print("The output should be coded as a 5-digit number consisting of 0s,1s, and 2s")
+                print("0 corresponds to a letter that is not in the word.")
+                print("1 corresponds to a letter that is in the word, but in the incorrect position.")
+                print("2 corresponds to a letter in the correct position.")
+
+        round_num += 1
